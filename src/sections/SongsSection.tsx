13cf968a5503d7878b5a3ae41e569bc8d3ad1d2b
rsharma441 <rsharma441@gmail.com>
2025-12-29 22:47:23 -0500

    fixed final 2 mobile

--- src/sections/SongsSection.tsx
+++ src/sections/SongsSection.tsx
@@ -6,24 +6,23 @@ const SongsSection: React.FC<{ songs: Song[] }> = ({ songs }) => {
   return (
     <div className="songs-block">
 
-        <div className="songs-content">
-            <p className="songs-intro">
-            To see my favorite tracks of 2025, check out this DJ mix of all of them 
-    
-            
-            </p>
-
-                    <a href="https://open.spotify.com/playlist/5ysHQMthqChqcx3ieV1Tp8?si=86c68003d6b44cb9" target="_blank" rel="noopener noreferrer">
-                here
-            </a>.
-            <br />
-            <br />
-            <p>
-                If you don't have Spotify, and actually want them, tell me hah. 
-            </p>
+<div className="songs-intro">
+  Listen to this{" "}
+  <a
+    href="https://open.spotify.com/playlist/5ysHQMthqChqcx3ieV1Tp8?si=86c68003d6b44cb9"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    this DJ mix
+  </a>{" "}
+  of my top 50 Songs of 2025! Enjoy!
+</div>
 
 
-        </div>
+      
+  
+
+
     </div>
   );
 };
